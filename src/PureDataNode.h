a69af41bbdcbca68948f2632fe239ebcac3882b8
Ryan Alexander <ryan@onecm.com>
2014-05-20 14:01:52 -0700

    Expose thread-safe array methods from PdBase

--- src/PureDataNode.h
+++ src/PureDataNode.h
@@ -32,6 +32,10 @@ public:
 	void sendList(const std::string& dest, const pd::List& list);
 	void sendMessage(const std::string& dest, const std::string& msg, const pd::List& list = pd::List());
 
+	bool readArray(const std::string& arrayName, std::vector<float>& dest, int readLen = -1, int offset = 0);
+	bool writeArray(const std::string& arrayName, std::vector<float>& source, int writeLen = -1, int offset = 0);
+	void clearArray(const std::string& arrayName, int value = 0);
+
 private:
 	pd::PdBase	mPdBase;
 	std::mutex	mMutex;
