78b7af62c2f1fa461fe406a2b8b9d52047755834
Ryan Alexander <ryan@onecm.com>
2015-06-15 01:29:32 -0700

    Communicate with PD on the audio thread via a task queue
    This commit is WIP and only partial functionality has been added. Still sorting out how to handle return values and incoming messages.

--- src/PureDataNode.h
+++ src/PureDataNode.h
@@ -7,42 +7,88 @@
 #include "cinder/audio/Node.h"
 #include "cinder/Thread.h"
 
+#include <queue>
+
 namespace cipd {
+
+namespace detail {
+
+template <typename T>
+class LockedQueue {
+  std::queue<T> mQueue;
+  mutable std::mutex mMutex;
+
+public:
+  void push(const T &item) {
+    std::lock_guard<std::mutex> lock(mMutex);
+    mQueue.push(item);
+  }
+
+  template <typename... Args>
+  void emplace(Args &&... args) {
+    std::lock_guard<std::mutex> lock(mMutex);
+    mQueue.emplace(std::forward<Args>(args)...);
+  }
+
+  T pop() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    auto item = std::move(mQueue.front());
+    mQueue.pop();
+    return item;
+  }
+
+  bool empty() const {
+    std::lock_guard<std::mutex> lock(mMutex);
+    return mQueue.empty();
+  }
+};
+
+} // detail
 
 typedef std::shared_ptr<pd::Patch> PatchRef;
 typedef std::shared_ptr<class PureDataNode> PureDataNodeRef;
 
 class PureDataNode : public ci::audio::Node {
+  pd::PdBase mPdBase;
+  size_t mNumTicksPerBlock;
+
+  ci::audio::BufferInterleaved mBufferInterleaved;
+
+  using Task = std::function<void(pd::PdBase &)>;
+  detail::LockedQueue<Task> mPendingTasks;
+
 public:
-	PureDataNode( const Format &format = Format() );
-	~PureDataNode();
+  PureDataNode(const Format &format = Format());
 
-	void initialize() override;
-	void uninitialize() override;
-	void process( ci::audio::Buffer *buffer );
+  void initialize() override;
+  void uninitialize() override;
+  void process(ci::audio::Buffer *buffer);
 
-	pd::PdBase& getPd()	{ return mPdBase; }
+//  pd::PdBase &getPd() {
+//    return mPdBase;
+//  }
 
-	PatchRef	loadPatch( ci::DataSourceRef dataSource );
-	void		closePatch( const PatchRef &patch );
+  void loadPatch(ci::DataSourceRef dataSource);
+  // void closePatch(const PatchRef &patch);
 
-	// thread-safe senders
-	void sendBang( const std::string& dest );
-	void sendFloat( const std::string& dest, float value );
-	void sendSymbol( const std::string& dest, const std::string& symbol );
-	void sendList( const std::string& dest, const pd::List& list );
-	void sendMessage( const std::string& dest, const std::string& msg, const pd::List& list = pd::List() );
+  void queueTask(Task &&task);
 
-	bool readArray( const std::string& arrayName, std::vector<float>& dest, int readLen = -1, int offset = 0 );
-	bool writeArray( const std::string& arrayName, std::vector<float>& source, int writeLen = -1, int offset = 0 );
-	void clearArray( const std::string& arrayName, int value = 0 );
+  void setReceiver(pd::PdReceiver* receiver);
+  void subscribe(const std::string &address);
 
-private:
-	pd::PdBase	mPdBase;
-	std::mutex	mMutex;
-	size_t		mNumTicksPerBlock;
+  // thread-safe senders
+  void sendBang(const std::string &dest);
+  void sendFloat(const std::string &dest, float value);
+  // void sendSymbol(const std::string &dest, const std::string &symbol);
+  // void sendList(const std::string &dest, const pd::List &list);
+  // void sendMessage(const std::string &dest, const std::string &msg,
+  //                  const pd::List &list = pd::List());
 
-	ci::audio::BufferInterleaved mBufferInterleaved;
+  // bool readArray(const std::string &arrayName, std::vector<float> &dest, int readLen = -1,
+  //                int offset = 0);
+  void writeArray(const std::string &name, std::vector<float> &source, int length = -1,
+                  int offset = 0);
+  void clearArray(const std::string &name, int value = 0);
 };
 
 } // namespace cipd
